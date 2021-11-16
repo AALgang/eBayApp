eb214d901f57090ab32fe5e05244f5bb6f440ed3
AudricPouzelgues <a.pouzelgues@gmail.com>
2021-11-16 15:09:54 +0100

    add nav bar into main activity

--- app/src/main/java/m2/miage/ebay/MainActivity.kt
+++ app/src/main/java/m2/miage/ebay/MainActivity.kt
@@ -1,15 +1,35 @@
 package m2.miage.ebay
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import m2.miage.ebay.home.HomeActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import m2.miage.myapplication.R
+import m2.miage.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        startActivity(Intent(this, HomeActivity::class.java))
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navView: BottomNavigationView = binding.navView
+
+            val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home, R.id.navigation_publish, R.id.navigation_profile
+            )
+        )
+        navView.setupWithNavController(navController)
     }
 }
