eea30827af6f0608fd4d24c90e490c7b16cec1a6
AudricPouzelgues <a.pouzelgues@gmail.com>
2021-11-16 14:09:42 +0100

    Start home activity

--- app/src/main/java/m2/miage/ebay/MainActivity.kt
+++ app/src/main/java/m2/miage/ebay/MainActivity.kt
@@ -1,11 +1,15 @@
 package m2.miage.ebay
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import m2.miage.ebay.home.HomeActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        startActivity(Intent(this, HomeActivity::class.java))
     }
 }
