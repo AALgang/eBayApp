e63773418ff71388cb51abaf0e23073c73c699d4
vivalx <alexis.vivier@toulouse.miage.fr>
2021-11-16 16:01:59 +0100

    Firebase: connexion Microsoft

--- app/src/main/java/m2/miage/ebay/FirebaseConnectActivity.kt
+++ app/src/main/java/m2/miage/ebay/FirebaseConnectActivity.kt
@@ -8,6 +8,7 @@ import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult
 import com.firebase.ui.auth.util.ExtraConstants
 import com.google.firebase.auth.ActionCodeSettings
 import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_firebase_connect.*
 
 class FirebaseConnectActivity : AppCompatActivity() {
 
@@ -23,7 +24,14 @@ class FirebaseConnectActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_firebase_connect)
-        createSignInIntent();
+
+        login.setOnClickListener {
+            createSignInIntent();
+        }
+
+        logout.setOnClickListener {
+            signOut();
+        }
     }
 
     private fun createSignInIntent() {
@@ -39,6 +47,7 @@ class FirebaseConnectActivity : AppCompatActivity() {
             .createSignInIntentBuilder()
             .setAvailableProviders(providers)
             .setLogo(R.drawable.my_great_logo) // Set logo drawable
+//            .setTheme(R.style.MySuperAppTheme) // Set theme
             .build()
         signInLauncher.launch(signInIntent)
         // [END auth_fui_create_intent]
@@ -78,77 +87,5 @@ class FirebaseConnectActivity : AppCompatActivity() {
                 // ...
             }
         // [END auth_fui_delete]
-    }
-
-    private fun themeAndLogo() {
-        val providers = emptyList<AuthUI.IdpConfig>()
-
-        // [START auth_fui_theme_logo]
-        val signInIntent = AuthUI.getInstance()
-            .createSignInIntentBuilder()
-            .setAvailableProviders(providers)
-            .setLogo(R.drawable.my_great_logo) // Set logo drawable
-//            .setTheme(R.style.MySuperAppTheme) // Set theme
-            .build()
-        signInLauncher.launch(signInIntent)
-        // [END auth_fui_theme_logo]
-    }
-
-    private fun privacyAndTerms() {
-        val providers = emptyList<AuthUI.IdpConfig>()
-        // [START auth_fui_pp_tos]
-        val signInIntent = AuthUI.getInstance()
-            .createSignInIntentBuilder()
-            .setAvailableProviders(providers)
-            .setTosAndPrivacyPolicyUrls(
-                "https://example.com/terms.html",
-                "https://example.com/privacy.html")
-            .build()
-        signInLauncher.launch(signInIntent)
-        // [END auth_fui_pp_tos]
-    }
-
-    open fun emailLink() {
-        // [START auth_fui_email_link]
-        val actionCodeSettings = ActionCodeSettings.newBuilder()
-            .setAndroidPackageName( /* yourPackageName= */
-                "...",  /* installIfNotAvailable= */
-                true,  /* minimumVersion= */
-                null)
-            .setHandleCodeInApp(true) // This must be set to true
-            .setUrl("https://google.com") // This URL needs to be whitelisted
-            .build()
-
-        val providers = listOf(
-            AuthUI.IdpConfig.EmailBuilder()
-                .enableEmailLinkSignIn()
-                .setActionCodeSettings(actionCodeSettings)
-                .build()
-        )
-        val signInIntent = AuthUI.getInstance()
-            .createSignInIntentBuilder()
-            .setAvailableProviders(providers)
-            .build()
-        signInLauncher.launch(signInIntent)
-        // [END auth_fui_email_link]
-    }
-
-    open fun catchEmailLink() {
-        val providers: List<AuthUI.IdpConfig> = emptyList()
-
-        // [START auth_fui_email_link_catch]
-        if (AuthUI.canHandleIntent(intent)) {
-            val extras = intent.extras ?: return
-            val link = extras.getString(ExtraConstants.EMAIL_LINK_SIGN_IN)
-            if (link != null) {
-                val signInIntent = AuthUI.getInstance()
-                    .createSignInIntentBuilder()
-                    .setEmailLink(link)
-                    .setAvailableProviders(providers)
-                    .build()
-                signInLauncher.launch(signInIntent)
-            }
-        }
-        // [END auth_fui_email_link_catch]
     }
 }
