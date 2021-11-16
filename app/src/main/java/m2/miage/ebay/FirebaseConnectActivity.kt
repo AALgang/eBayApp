2bdbb92ce86d1343202b9cb1b9c145c5a1faa648
AudricPouzelgues <a.pouzelgues@gmail.com>
2021-11-16 17:22:29 +0100

    Start with login screen and continue to home page

--- app/src/main/java/m2/miage/ebay/FirebaseConnectActivity.kt
+++ app/src/main/java/m2/miage/ebay/FirebaseConnectActivity.kt
@@ -1,41 +1,28 @@
 package m2.miage.ebay
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
 import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult
-import com.firebase.ui.auth.util.ExtraConstants
-import com.google.firebase.auth.ActionCodeSettings
 import com.google.firebase.auth.FirebaseAuth
-import kotlinx.android.synthetic.main.activity_firebase_connect.*
 
 class FirebaseConnectActivity : AppCompatActivity() {
 
-    // [START auth_fui_create_launcher]
-    // See: https://developer.android.com/training/basics/intents/result
     private val signInLauncher = registerForActivityResult(
         FirebaseAuthUIActivityResultContract()
     ) { res ->
         this.onSignInResult(res)
     }
-    // [END auth_fui_create_launcher]
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_firebase_connect)
-
-        login.setOnClickListener {
-            createSignInIntent();
-        }
-
-        logout.setOnClickListener {
-            signOut();
-        }
+        createSignInIntent()
     }
 
     private fun createSignInIntent() {
-        // [START auth_fui_create_intent]
         // Choose authentication providers
         val providers = arrayListOf(
             AuthUI.IdpConfig.EmailBuilder().build(),
@@ -46,8 +33,8 @@ class FirebaseConnectActivity : AppCompatActivity() {
         val signInIntent = AuthUI.getInstance()
             .createSignInIntentBuilder()
             .setAvailableProviders(providers)
+            .setTheme(R.style.Theme_Ebay)
             .setLogo(R.drawable.my_great_logo) // Set logo drawable
-//            .setTheme(R.style.MySuperAppTheme) // Set theme
             .build()
         signInLauncher.launch(signInIntent)
         // [END auth_fui_create_intent]
@@ -59,33 +46,26 @@ class FirebaseConnectActivity : AppCompatActivity() {
         if (result.resultCode == RESULT_OK) {
             // Successfully signed in
             val user = FirebaseAuth.getInstance().currentUser
-            // ...
+            startActivity(Intent(this, StartActivity::class.java))
         } else {
-            // Sign in failed. If response is null the user canceled the
-            // sign-in flow using the back button. Otherwise check
-            // response.getError().getErrorCode() and handle the error.
-            // ...
+
         }
     }
-    // [END auth_fui_result]
 
     private fun signOut() {
-        // [START auth_fui_signout]
+
         AuthUI.getInstance()
             .signOut(this)
             .addOnCompleteListener {
                 // ...
             }
-        // [END auth_fui_signout]
     }
 
     private fun delete() {
-        // [START auth_fui_delete]
         AuthUI.getInstance()
             .delete(this)
             .addOnCompleteListener {
                 // ...
             }
-        // [END auth_fui_delete]
     }
 }
---
+++ app/src/main/java/m2/miage/ebay/StartActivity.kt
@@ -0,0 +1,32 @@
+package m2.miage.ebay
+
+import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import m2.miage.ebay.databinding.ActivityStartBinding
+
+class StartActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityStartBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityStartBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navView: BottomNavigationView = binding.navView
+
+        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+
+        AppBarConfiguration(
+            setOf(
+                R.id.navigation_home, R.id.navigation_publish, R.id.navigation_profile
+            )
+        )
+        navView.setupWithNavController(navController)
+    }
+}
