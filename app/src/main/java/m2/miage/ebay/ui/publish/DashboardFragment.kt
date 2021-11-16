d51436769e0919745ae895ff38c2c9124f9a6e38
AudricPouzelgues <a.pouzelgues@gmail.com>
2021-11-16 15:12:33 +0100

    add all fragments

---
+++ app/src/main/java/m2/miage/ebay/ui/publish/DashboardFragment.kt
@@ -0,0 +1,19 @@
+package m2.miage.ebay.ui.publish
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import m2.miage.myapplication.R
+
+class DashboardFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        return inflater.inflate(R.layout.fragment_profile, container, false)
+    }
+}
