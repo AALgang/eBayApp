364c91d92e5ffa53621838056173e7974082929b
vivalx <alexis.vivier@toulouse.miage.fr>
2021-11-18 09:47:42 +0100

    Add user to database

--- app/src/main/java/m2/miage/ebay/FirebaseConnectActivity.kt
+++ app/src/main/java/m2/miage/ebay/FirebaseConnectActivity.kt
@@ -3,10 +3,13 @@ package m2.miage.ebay
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
 import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class FirebaseConnectActivity : AppCompatActivity() {
 
@@ -42,13 +45,59 @@ class FirebaseConnectActivity : AppCompatActivity() {
 
     // [START auth_fui_result]
     private fun onSignInResult(result: FirebaseAuthUIAuthenticationResult) {
+
         val response = result.idpResponse
         if (result.resultCode == RESULT_OK) {
             // Successfully signed in
             val user = FirebaseAuth.getInstance().currentUser
+
+            user?.let {
+                // Name, email address
+                val name = user.displayName
+                val email = user.email
+
+                if (email != null && name != null) {
+                    addUser(name, email, user.uid)
+                }
+            }
+
+            // Launch acticity
             startActivity(Intent(this, StartActivity::class.java))
+            this.finish();
         } else {
 
+        }
+    }
+
+    private fun addUser(mail:String, name:String, pseudo:String){
+        var exist = false;
+        // Access a Cloud Firestore instance from your Activity
+        val db = Firebase.firestore
+
+        db.collection("users").whereEqualTo("email", mail)
+            .get().addOnSuccessListener { document ->
+                if(document.isEmpty){
+                    exist = true;
+                }
+            }
+
+        if(!exist) {
+            // Create a new user with a first and last name
+            val user = hashMapOf(
+                "mail" to mail,
+                "name" to name,
+                "pseudo" to pseudo
+            )
+
+            // Add a new document with a generated ID
+            db.collection("users")
+                .add(user)
+                .addOnSuccessListener { documentReference ->
+                    Log.d("Add", "DocumentSnapshot added with ID: ${documentReference.id}")
+                }
+                .addOnFailureListener { e ->
+                    Log.w("Add", "Error adding document", e)
+                }
         }
     }
 
