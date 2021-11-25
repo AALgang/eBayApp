2c1682a7c012ac970f5a2d7cb2bc42f02ca87e94
vivalx <alexis.vivier@toulouse.miage.fr>
2021-11-25 12:11:02 +0100

    Mise à jour de la position TODO: Probléme quand on refuse

--- app/src/main/java/m2/miage/ebay/ui/profile/ProfileFragment.kt
+++ app/src/main/java/m2/miage/ebay/ui/profile/ProfileFragment.kt
@@ -1,27 +1,64 @@
 package m2.miage.ebay.ui.profile
 
-import android.content.Intent
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
-import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
-import com.firebase.ui.auth.AuthUI
 import kotlinx.android.synthetic.main.fragment_profile.*
 import m2.miage.ebay.FirebaseConnectActivity
 import m2.miage.ebay.R
+import android.location.LocationListener
+import android.location.LocationManager
+import android.os.Build
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+
 
 class ProfileFragment : Fragment() {
 
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+
+    @RequiresApi(Build.VERSION_CODES.N)
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(activity)
+        val locationPermissionRequest = registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions()
+        ) { permissions ->
+            when {
+                permissions.getOrDefault(Manifest.permission.ACCESS_FINE_LOCATION, false) -> {
+                    // Precise location access granted.
+                }
+                permissions.getOrDefault(Manifest.permission.ACCESS_COARSE_LOCATION, false) -> {
+                    // Only approximate location access granted.
+                } else -> {
+                // No location access granted.
+            }
+            }
+        }
+
+        locationPermissionRequest.launch(arrayOf(
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION))
         return inflater.inflate(R.layout.fragment_profile, container, false)
     }
 
+    @RequiresApi(Build.VERSION_CODES.N)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
@@ -29,5 +66,50 @@ class ProfileFragment : Fragment() {
             FirebaseConnectActivity.signOut(requireContext())
             activity?.finish()
         }
+
+        btn_localiser.setOnClickListener {
+            updateLocation()
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    private fun updateLocation() {
+        if (context?.let {
+                ActivityCompat.checkSelfPermission(
+                    it,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                )
+            } != PackageManager.PERMISSION_GRANTED && context?.let {
+                ActivityCompat.checkSelfPermission(
+                    it,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                )
+            } != PackageManager.PERMISSION_GRANTED
+        ) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            Toast.makeText(requireContext(), "Vous devez autoriser l'accés a la position", Toast.LENGTH_LONG)
+        }
+        fusedLocationClient.lastLocation
+            .addOnSuccessListener { location: Location? ->
+                // Add a new document with a generated ID
+                FirebaseAuth.getInstance().currentUser?.let {
+                    if (location != null) {
+                        var coordonnees = location.longitude.toString() + "," + location.longitude.toString()
+                        Firebase.firestore.collection("users").document(it.uid)
+                            .update("location", coordonnees)
+                            .addOnSuccessListener { Log.d("FB", "DocumentSnapshot successfully updated!") }
+                            .addOnFailureListener { e -> Log.w("FB", "Error updating document", e) }
+                    }
+                }
+            }
+            .addOnFailureListener{
+                Toast.makeText(requireContext(), "Vous devez autoriser l'accés a la position", Toast.LENGTH_LONG)
+            }
     }
 }
